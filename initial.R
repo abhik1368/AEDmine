d7db051ca0ede09d7c128ef2664bccdb96669ff0
abhik1368 <abhik1368@gmail.com>
2014-12-18 17:35:03 -0500

    Update some variables

--- initial.R
+++ initial.R
@@ -17,6 +17,7 @@ indication <-read.table("indi12q4.txt",sep = "$", header = T, fill = T, quote = 
 
 # Setting the brand names of drugs 
 # Create a dataframe of same brand name drugs with Drugbank ID 
+# You have to email me @ abhik1368@gmail.com for below datasets
 dbclass<-read.csv("dbatc.csv",header=TRUE,stringsAsFactors=FALSE)
 dnames<-read.csv("brandnames.csv",header=TRUE,stringsAsFactors=FALSE)
 dn<-gsub("\n","",dnames$Name)
@@ -89,31 +90,24 @@ adrs.b<-adrs[adrs$atc_code=="B",]
 adrs.n<-adrs[adrs$atc_code=="N",]
 adrs.a<-adrs[adrs$atc_code=="A",]
 
+# Add columns and class names .
 adrs.b$drug<-"Blood and blood forming organs"
 adrs.n$drug<- "Nervous system"
 adrs.a$drug<- "Alimentary tract and metabolism"
 #adrs<-rbind(adrs.b[1:50,],adrs.n[1:50,],adrs.a[1:50,])
 
 ## Plot only the ATC code B with Aspirin
+## A user might change to different classes like N or A
 adrs.a$atc_code<-NULL
 ## Plot Pyramid charts
 adrs.a$count= -1 * adrs.a$count
 
 ## combine two dataframe and look for the top reactions
 
-new.df<-rbind(adrs.b[1:55,],n1.df[1:55,])
+new.df<-rbind(adrs.a[1:55,],n1.df[1:55,])
 new.df<-new.df[order(new.df$count, decreasing = T), ]
 new.df$count<-as.numeric(new.df$count)
 new.df$drug<-as.factor(new.df$drug)
 
 p1<-pyramidPlot(new.df,colors=c("blue","red"))
 
-
-## Checking on indications
-colnames(indication)[3]<-"drug_seq"
-df<-merge(df,indication,by=c("primaryid","caseid","drug_seq"))
-df2 <- as.data.frame(table(df$indi_pt))
-names(df2) <- c("indication", "count")
-df2 <- df2[order(df2$count, decreasing = T), ]
-n.df<-df2[df2$count > 0,]
-head(n.df)
