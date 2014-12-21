3c838aadd2d16a533685f342ceb7b6d27cdb51d4
Abhik Seal <abhik1368@gmail.com>
2014-12-21 13:49:06 -0600

    update functions

--- pyramidPlot.R
+++ pyramidPlot.R
@@ -4,41 +4,36 @@ library(rCharts)
 
 ## Plot pyramid plots usinf NDV3 javascript library
 pyramidPlot <- function(data.df,colors=NULL){
-  
+  library(rCharts)
   n1 <- nPlot(
-    y = 'count', 
-    x = 'reaction', 
+    y = 'Count',
+    x = 'reaction',
     group = 'drug',
-    type = 'multiBarHorizontalChart', 
+    type = 'multiBarHorizontalChart',
     data = data.df
-  )
-  
+    )
   n1$chart(stacked = TRUE)
   n1$chart(tooltipContent = "#! function(key, x, y){
-           var format = d3.format('0,000');
-           return '<h3>' + key + ', Adverse reaction : ' + x + '</h3>' + 
-           '<p>' + 'Counts : ' + y + '</p>'
-} !#")
-  
-  
-  if (max(new.df$count >= 20)) {    
-    n1$yAxis(axisLabel = "Count",  
+   var format = d3.format('0,000');
+   return '<h3>' + key + ', Adverse reaction : ' + x + '</h3>' +
+   '<p>' + 'Counts : ' + y + '</p>'
+   } !#")
+  if (max(new.df$count >= 20)) {
+    n1$yAxis(axisLabel = "Count",
              tickFormat = "#! function(d) {
-                          return d3.format(',.1f')(Math.abs(d) / 1)
-                          } !#")
-  } else {
-    n1$yAxis(axisLabel = "Count",  
-             tickFormat = "#! function(d) {
-                          return d3.format(',.0f')(Math.abs(d) / 1)
-                          } !#")    
-    
-  }
-  
-  if (!is.null(colors)) {
-    n1$chart(color = colors)
-  }
-  n1$chart(stacked = TRUE)
-  n1$set(height=800,width=650)
-  n1$chart(margin = list(left=200)) 
-  n1
-  }
+            return d3.format(',.1f')(Math.abs(d) / 1)
+             } !#")
+    } else {
+      n1$yAxis(axisLabel = "Count",
+               tickFormat = "#! function(d) {
+               return d3.format(',.0f')(Math.abs(d) / 1)
+               } !#")
+    }
+   if (!is.null(colors)) {
+     n1$chart(color = colors)
+     }
+   n1$chart(stacked = TRUE)
+   n1$set(height=800,width=650)
+   n1$chart(margin = list(left=200))
+   n1
+}
