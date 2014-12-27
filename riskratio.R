26be97642afe20ac483788eaaf9f965b00aa0c9e
abhik1368 <abhik1368@gmail.com>
2014-12-27 16:04:09 -0600

    update function
    ror function added.

--- riskratio.R
+++ riskratio.R
@@ -1,8 +1,8 @@
-# Function to calculate Risk Ratio
+# Function to calculate proportional risk ration
 # param d1: dataframe containing side effects for drug 1
 # param d2: dataframe containing side effects for drug 2
 
-riskratio <- function (d1,d2){
+prr<- function (d1,d2){
   rat<-data.frame()
   for (i in 1:dim(d2)[1]){
     r<-as.character(d2$reaction[i])
@@ -12,12 +12,37 @@ riskratio <- function (d1,d2){
     t2<-sum(d2$count)
     if ( length(c1) > 0){
       #ratio<-c1/c2
-      ratio<-(c1/t1)/(c2/t2)
-      advr<-cbind(r,ratio)
+      prr<-(c1/t1)/(c2/t2)
+      advr<-cbind(r,prr)
       #print (advr)
       rat<-rbind(rat,advr)
-      rat$ratio<-as.numeric(rat$ratio)
-      tab<-rat[order(rat$ratio, decreasing = T), ]
+      rat$prr<-as.numeric(rat$prr)
+      tab<-rat[order(rat$prr, decreasing = T), ]
+      colnames(tab)[1]<-"reaction"
+    }   
+  }
+  return (tab)
+}
+
+# Function to calculate reporting odds ratio
+# param d1: dataframe containing side effects for drug 1
+# param d2: dataframe containing side effects for drug 2
+
+ror <- function (d1,d2){
+  rat<-data.frame()
+  for (i in 1:dim(d2)[1]){
+    r<-as.character(d2$reaction[i])
+    c2 <-d2$count[i]
+    c1<-d1[d1$reaction==r,]$count
+    t1<-sum(d1$count)-c1
+    t2<-sum(d2$count)-c2
+    if ( length(c1) > 0){
+      ror<-(c1/c2)/(t1/t2)
+      advr<-cbind(r,ror)
+      rat<-rbind(rat,advr)
+      rat$ror<-as.numeric(rat$ror)
+      tab<-rat[order(rat$ror, decreasing = T), ]
+      colnames(tab)[1]<-"reaction"
     }   
   }
   return (tab)
--- run.R
+++ run.R
@@ -23,11 +23,12 @@ t<-d1se[which(d$reaction %in% d1se$reaction),]
 lipitor<-aggregate(count ~ reaction ,data=t, FUN=sum)
 
 # compare crestor against Lipitor 
-r1<-riskratio(crestor,lipitor)
+r1.prr<-prr(crestor,lipitor)
+r1.ror<-ror(crestor,lipitor)
 # Write top 10 reactions output as xtable html format
-print(xtable(head(r1,10)) , type="html",file="compare_results.html")
+print(xtable(head(r1,10)) , type="html",file="compare_results1.html")
 
 # compare lipitor against crestor 
 r2<-riskratio(lipitor,crestor)
 # Write top 10 reactions output as xtable html format
-print(xtable(head(r1,10)), type="html",file="compare_results.html")
+print(xtable(head(r2,10)), type="html",file="compare_results2.html")
